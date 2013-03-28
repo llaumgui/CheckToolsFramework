40280ab358063acffd278ad6893a1404ae019420
Vieillard <baptiste_vieillard@hotmail.com>
2013-03-28 17:19:55 +0100

    Last comit mise a jour json/yml

---
+++ lib/test/qattestyml.php
@@ -0,0 +1,57 @@
+<?php
+/**
+ * File containing the qatTestYml class.
+ *
+ * @version //autogentag//
+ * @package QATools
+ * @copyright Copyright (C) 2012 Guillaume Kulakowski and contributors
+ * @license http://www.gnu.org/licenses/gpl-2.0.html GNU General Public License v2.0
+ */
+
+/**
+ * The qatTestYml class.
+ *
+ * Provide tests for php file.
+ *
+ * @package QATools
+ * @version //autogentag//
+ */
+
+/*  Pour version autoload
+ *	use Symfony\Component\Yaml\Yaml;
+ */
+
+use Symfony\Component\Yaml\Yaml;
+
+class qatTestYml extends qatTest
+{
+
+    /**
+     * Check if YML is valid
+     *
+     * @param qatJunitXMLTestSuite $testSuite
+     * @param string $file
+     * @param string $yml
+     */
+    public static function checkValidity( qatJunitXMLTestSuite &$testSuite, $file , $yml )
+    {
+        $ct = qatConsoleTools::getInstance();
+
+        $checkValidity = $testSuite->addTest();
+        $checkValidity->setName( 'Check if YML is valid' );
+        $checkValidity->setFile( $file );
+        $checkValidity->setAssertions( 1 );
+        
+        try {
+        	Yaml::parse($yml); //try to parse 
+        } catch (Exception $e) {
+        	$message = $e->getMessage();
+        	$checkValidity->addFaillure( 'YML', $message );
+            $ct->output->outputLine( $message, 'error' );
+        }
+        
+        $checkValidity->finish();
+    }
+}
+
+?>
