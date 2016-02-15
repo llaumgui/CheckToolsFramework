ea63c2c905c06eee6f82ebb96509f0a734594bf9
Guillaume Kulakowski <guillaume@kulakowski.fr>
2016-02-15 21:41:34 +0100

    First TU !

---
+++ tests/CheckTool/BomCheckToolTest.php
@@ -0,0 +1,45 @@
+<?php
+
+/*
+ * This file is part of the CheckToolsFramework package.
+ *
+ * Copyright (C) 2015-2016 Guillaume Kulakowski <guillaume@kulakowski.fr>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Tests\Llaumgui\CheckToolsFramework\CheckTool;
+
+use Llaumgui\CheckToolsFramework\CheckTool\BomCheckTool;
+use Symfony\Component\Finder\Finder;
+
+/**
+ * The service BomCheckTool.
+ */
+class BomCheckToolTest extends \PHPUnit_Framework_TestCase {
+
+    /**
+     * Check if content has BOM.
+     */
+    public function testDoCheck() {
+
+        $bomCheckTool = new BomCheckTool();
+
+        // Get testing files
+        $finder = new Finder();
+        $finder->files()->in(__DIR__ . '/../files');
+
+        foreach ($finder as $file) {
+            $check = $bomCheckTool->doCheck($file);
+            if(strpos($file->getFileName(), "bom_ko") !== false) {
+                $this->assertFalse($check->getResult());
+            } else {
+                $this->assertTrue($check->getResult());
+            }
+
+            $this->assertInstanceOf('Llaumgui\CheckToolsFramework\CheckTool\CheckToolTest', $check);
+        }
+    }
+
+}
