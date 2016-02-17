528cfeeafd9d8032936817223db539e7663a4dbb
Guillaume Kulakowski <guillaume@kulakowski.fr>
2016-02-17 21:25:21 +0100

    [Issue #8] Build Phar from Travis.

--- generate-website.sh
+++ generate-website.sh
@@ -1,30 +1,37 @@
 #!/bin/sh
 
+BUILD_PATH=gh-pages
+
 # Cleanup gh-pages
-rm -rf gh-pages
-mkdir gh-pages
+rm -rf ${BUILD_PATH}
+mkdir ${BUILD_PATH}
 
 # Get ApiGen.phar and Couscous
 wget http://www.apigen.org/apigen.phar
 wget http://couscous.io/couscous.phar
 
 # Generate documentation
-php couscous.phar generate --target gh-pages
+php couscous.phar generate --target ${BUILD_PATH}
 
 # Generate Api
 php apigen.phar generate
+
+# Build Phar
+bin/compile
+mv phpct.phar ${BUILD_PATH}
+mv phpct.version ${BUILD_PATH}
 
 # Set identity
 git config --global user.email "travis@travis-ci.org"
 git config --global user.name "Travis"
 
 # Add branch
-cd ./gh-pages
+cd ./${BUILD_PATH}
 git init
 git remote add origin https://${GH_TOKEN}@github.com/llaumgui/CheckToolsFramework.git > /dev/null
-git checkout -B gh-pages
+git checkout -B ${BUILD_PATH}
 
 # Push generated files
 git add .
 git commit -m "Doc updated"
-git push origin gh-pages -fq > /dev/null
+git push origin ${BUILD_PATH} -fq > /dev/null
